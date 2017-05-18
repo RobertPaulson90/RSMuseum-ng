9a4f6cf309bf2b0cd8f86a70c8116e9977e10c1f
René Sørensen <rensoe@gmail.com>
2017-05-18 11:12:55 +0200

    Add registration filter by volunteer name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
-import {AppComponent} from './app.component';
-import {RegistrationListComponent} from './registration/registration-list/registration-list.component';
-import {RegistrationItemComponent} from './Registration/Registration-List/registration-item/registration-item.component';
-import {RegistrationFormComponent} from './Registration/registration-form/registration-form.component';
-import {HeaderComponent} from './header/header.component';
-import {HomeComponent} from './home/home.component';
-import {VolunteerListComponent} from './volunteer/volunteer-list/volunteer-list.component';
-import {VolunteerItemComponent} from './volunteer/volunteer-list/volunteer-item/volunteer-item.component';
-import {SearchComponent} from './shared/search/search.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HelperService} from './shared/helper.service';
-import {RegistrationService} from './registration/registration.service';
-import {DatepickerModule} from './shared/angular2-material-datepicker/src/datepicker.module';
-import {ErrorPageComponent} from './shared/error-page/error-page.component';
-import {AppRoutingModule} from './app-routing.module';
-import {VolunteerService} from './volunteer/volunteer.service';
+import { AppComponent } from './app.component';
+import { RegistrationListComponent } from './registration/registration-list/registration-list.component';
+import { RegistrationItemComponent } from './Registration/Registration-List/registration-item/registration-item.component';
+import { RegistrationFormComponent } from './Registration/registration-form/registration-form.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { VolunteerListComponent } from './volunteer/volunteer-list/volunteer-list.component';
+import { VolunteerItemComponent } from './volunteer/volunteer-list/volunteer-item/volunteer-item.component';
+import { SearchComponent } from './shared/search/search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HelperService } from './shared/helper.service';
+import { RegistrationService } from './registration/registration.service';
+import { DatepickerModule } from './shared/angular2-material-datepicker/src/datepicker.module';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { VolunteerService } from './volunteer/volunteer.service';
 import { VolunteerFilterByNamePipe } from './shared/volunteer-filter.pipe';
+import { RegistrationFilterByVolunteerNamePipe } from './shared/registration-filter.pipe';
 
 
 @NgModule({
@@ -35,7 +36,8 @@ import { VolunteerFilterByNamePipe } from './shared/volunteer-filter.pipe';
     VolunteerItemComponent,
     SearchComponent,
     ErrorPageComponent,
-    VolunteerFilterByNamePipe
+    VolunteerFilterByNamePipe,
+    RegistrationFilterByVolunteerNamePipe
   ],
   imports: [
     BrowserModule,
