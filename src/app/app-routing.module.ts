daef797d1d7dd8f86ad88610d0bf11f51f5f2c75
Amivit <poul1446@edu.eal.dk>
2017-05-22 13:01:22 +0200

    StatisticsComponent started and fixed a typo on header list items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import {HomeComponent} from './home/home.component';
 import {VolunteerListComponent} from './volunteer/volunteer-list/volunteer-list.component';
 import {RegistrationListComponent} from './registration/registration-list/registration-list.component';
 import {ErrorPageComponent} from './shared/error-page/error-page.component';
+import {StatisticsComponent} from './statistics/statistics.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'frivillige', component: VolunteerListComponent },
   { path: 'registrering-godkendelse', component: RegistrationListComponent },
+  { path: 'statistik', component: StatisticsComponent },
   { path: 'ikke-fundet-404', component: ErrorPageComponent, data: {message: 'Beklager. Siden findes ikke!'} },
   { path: '**', redirectTo: '/ikke-fundet-404' }
 ];
